8d42d3f6eb18ba8d2318edcceddc19f49776c999
Daniel Steil <daniel.steil@hotmail.com>
2020-07-08 16:48:48 -0300

    vai

--- src/main/java/br/com/ottimizza/salasvirtuais/controllers/SalaController.java
+++ src/main/java/br/com/ottimizza/salasvirtuais/controllers/SalaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +33,7 @@ public class SalaController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<?> salvaSala(SalaDTO sala) throws Exception {
+	public ResponseEntity<?> salvaSala(@RequestBody SalaDTO sala) throws Exception {
 		return ResponseEntity.ok(salaService.salvaSala(sala));
 	}
 	
